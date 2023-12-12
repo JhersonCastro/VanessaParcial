543fbe7a389d37ee3f04eb168f5aa6f59451924c
Bucket <jhersonmartinez2005@gmail.com>
2023-12-12 12:30:53 -0500

    Nya ichi ni san, nya arigato

--- ProductManagerFinal/src/Users/IUser.java
+++ ProductManagerFinal/src/Users/IUser.java
@@ -4,5 +4,5 @@ import Company.Inventory;
 
 public interface IUser {
     public void login(Inventory inventory);
-        void updateProfile(String name, String email);
+    public void updateProfile(String name, String email);
 }
